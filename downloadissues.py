bd242dc04a4f0fc0c2837bbc3506429603693a9d
Joe Reddington <joe@joereddington.com>
2017-02-10 15:41:55 +0000

    basic authentication working

--- downloadissues.py
+++ downloadissues.py
@@ -1,9 +1,18 @@
 import urllib, json
+from urllib2 import urlopen, Request
+
+config = json.loads(open('config.json').read())
+token = config["token"]
+
+print token
+
 target = 'joereddington/Vision'
 url = 'https://api.github.com/repos/%s/issues' % target
 print url
-response = urllib.urlopen(url)
+request=Request(url)
+request.add_header('Authorization', 'token %s' % token)
+response = urlopen(request)
 data = json.loads(response.read())
-print data
+#print data
 for i in data:
     print i["title"]
