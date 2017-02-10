ff8ebc7e6d1c8525c503769bd69aedac2f07720c
Joe Reddington <joe@joereddington.com>
2017-02-10 16:15:01 +0000

    Downloading a test file

--- downloadissues.py
+++ downloadissues.py
@@ -1,14 +1,36 @@
 import urllib, json
 from urllib2 import urlopen, Request
+import re
 
-def get_issues(target):
-    url = 'https://api.github.com/repos/%s/issues' % target
+def get_json_from_url(url):
     config = json.loads(open('config.json').read())
     token = config["token"]
     request=Request(url)
     request.add_header('Authorization', 'token %s' % token)
     response = urlopen(request)
     return json.loads(response.read())
+
+def get_issues(target):
+    url = 'https://api.github.com/repos/%s/issues' % target
+    return get_json_from_url(url)
+
+def download_comment_to_file(title,url):
+    data =get_json_from_url(url)
+    with open(slugify(unicode(title))+'.md', 'wb') as target_file:
+        for comment in data:
+             target_file.write(comment['body'])
+
+#via http://stackoverflow.com/a/295466/170243 and Django
+def slugify(value):
+    """
+    Normalizes string, converts to lowercase, removes non-alpha characters,
+    and converts spaces to hyphens.
+    """
+    import unicodedata
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
+    value = unicode(re.sub('[-\s]+', '-', value))
+    return value
 
 
 target = 'joereddington/Vision'
@@ -19,6 +41,9 @@ data.extend(get_issues('equalitytime/home'))
 data = sorted(data, key=lambda k: k['title'])
 for i in data:
     print i["title"]
-
+comments_list=[]
 for i in data:
-    print i["comments_url"]
+    comments_list.extend( i["comments_url"])
+
+
+download_comment_to_file("test","https://api.github.com/repos/joereddington/Vision/issues/1/comments")
