45c387ee5d73b0e7f12818c1ed64dbd5381eef6d
Joe Reddington <joe@joereddington.com>
2017-02-10 15:53:13 +0000

    now sorts keys from multiple repositries

--- downloadissues.py
+++ downloadissues.py
@@ -1,18 +1,21 @@
 import urllib, json
 from urllib2 import urlopen, Request
 
-config = json.loads(open('config.json').read())
-token = config["token"]
+def get_issues(target):
+    url = 'https://api.github.com/repos/%s/issues' % target
+    config = json.loads(open('config.json').read())
+    token = config["token"]
+    request=Request(url)
+    request.add_header('Authorization', 'token %s' % token)
+    response = urlopen(request)
+    return json.loads(response.read())
 
-print token
 
 target = 'joereddington/Vision'
-url = 'https://api.github.com/repos/%s/issues' % target
-print url
-request=Request(url)
-request.add_header('Authorization', 'token %s' % token)
-response = urlopen(request)
-data = json.loads(response.read())
-#print data
+data = get_issues(target)
+data.extend(get_issues('equalitytime/whitewaterwriters'))
+data.extend(get_issues('equalitytime/home'))
+
+data = sorted(data, key=lambda k: k['title'])
 for i in data:
     print i["title"]
