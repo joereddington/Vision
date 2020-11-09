a1f4a069738f7c5ef597287bf5720a1e398d427c
Joseph Reddington <joe@joereddington.com>
2020-11-09 20:51:42 +0000

    I've added a is_repo_private method.
    The goal is to have vision give the title of the project to work on, but
    overall that's a fairly small THING because a) I know if it can be
    streamed or not, and b) I shouldn't tempt myself to ignore projects that
    I should work on.

--- vision.py
+++ vision.py
@@ -19,22 +19,33 @@ def get_json_from_url(url):
 def process_cards(pri,url, tag=""):
    cards= get_json_from_url(url)
    for card in cards: 
+       print(card)
        payload=""
        if card['note']:
             payload="map project:"+ card['note']
        else:
-            payload="Work on: "+ card['content_url'].replace("api.github.com/repos","github.com")
+                payload="Work on: "+ card['content_url'].replace("api.github.com/repos","github.com")
 
-       print("({}) {} {}".format(pri,payload,tag))
+       if "F" not in pri: 
+           print("({}) {} {}".format(pri,payload,tag))
    
+
+def is_repo_private(repo_url):
+   content=get_json_from_url(repo_url)
+   return content['private']
 
 def process_project_board(url,tag=""):
    board= get_json_from_url(url)
    columns_url= board["columns_url"]
    columns= get_json_from_url(columns_url)
-   priorities=['*', 'A', 'B', 'C', 'D','E', "F", "F", "F"]
+   priorities=['.', 'A', 'B', 'C', 'D','F', "F", "F", "F"]
    for x in columns:
       process_cards(priorities.pop(0),x['cards_url'],tag)
+
+def for_tests():
+   print(is_repo_private("https://api.github.com/repos/eQualityTime/Home"))
+   print(is_repo_private("https://api.github.com/repos/eQualityTime/Public"))
+
 
 if __name__ == "__main__":
    process_project_board("https://api.github.com/projects/1613733","+EQT")
